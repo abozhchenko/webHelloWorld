d41a7b25b1b95e8a1262f9b343d3b8b5f5b0853a
lexal <a.bozhchenko@gmail.com>
2020-04-17 16:33:15 +0300

    init commit2

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo mkdir -p /opt/docker/webHelloWorld/resources/
+sudo cp .//resources/response.json /opt/docker/webHelloWorld/resources/
+sudo chmod -R 777 /opt/docker/webHelloWorld/
+
+docker run --rm --name web -p 8080:8080 -v /opt/docker/webHelloWorld/resources/:/opt/docker/app/resources/ webHelloWorld
+
